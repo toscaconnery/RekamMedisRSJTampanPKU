32b437c4daf0c13c90a57f10ba940ecca60fae3e
DESKTOP-KI75MOF\USER <auliasyauki@gmail.com>
2018-09-04 21:08:38 +0800

    3 pdf complete again yey

---
+++ resources/views/doc_ri_informasi.blade.php
@@ -0,0 +1,127 @@
+<?php 
+header('Content-Type: application/pdf');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body style="">
+
+<h2 align="center" style="font-size:100%">PEMBERIAN INFORMASI</h2><br>
+
+<table>
+  <thead>
+    <tr>
+     <th class="big-box" width="15%">TANGGAL & JAM</th> 
+     <th class="big-box" width="20%">Informasi Yang disamp </th> 
+     <th class="big-box" width="45%">Pemberi Informasi</th> 
+     <th class="big-box" width="20%">VERIFIKASI DPJP</th> 
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+     <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+   <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">
+        <b>S - </b>
+        <br><b>O -</b>
+        <br><b>A -</b>
+        <br><b>P -</b>
+        <br>-
+      </td>
+      <td class="box-c">-</td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+
+<html>
---
+++ resources/views/doc_ri_monitoring.blade.php
@@ -0,0 +1,107 @@
+<?php 
+header('Content-Type: application/pdf');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body style="">
+
+<h2 align="center" style="font-size:100%">LEMBAR MONITORING & OBSERVASI PASIEN KHUSUS</h2><br>
+
+<table>
+  <thead>
+    <tr>
+     <th class="big-box" width="7%">Tgl</th>
+     <th class="big-box" width="7%">Jam</th> 
+     <th class="big-box" width="31%">Hasil Monitoring</th> 
+     <th class="big-box" width="31%">Implementasi</th> 
+     <th class="big-box" width="14%">Paraf &<br> Nama<br> Perawat</th> 
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+    <tr>
+      <td class="box-c">-</td>
+      <td class="box-c">-</td>
+      <td class="box">-</td>
+      <td class="box">-</td>
+      <td class="box-c"><br><br><br><br><br></td>
+    </tr>
+  </tbody>
+</table>
+  
+<p style="font-size:80%">
+  1. Restrain
+  <br>2. Resiko Jatuh
+  <br>3. Bermasalah Fisik
+</p>
+
+</body>
+<html>
