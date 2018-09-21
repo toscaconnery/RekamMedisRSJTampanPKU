3a6f34ecbea4e0d780a535640a2f79a89f6afaf5
DESKTOP-KI75MOF\USER <auliasyauki@gmail.com>
2018-09-21 22:31:08 +0800

    css fix for persetujuan_pdf

--- resources/views/test_pdf.blade.php
+++ resources/views/test_pdf.blade.php
@@ -96,7 +96,7 @@
                     <td>
                       <div class="btn-group">
                         <a class="btn btn-primary" href="{{url('')}}/pdf_rj_stressor"><i class="icon_plus_alt2"></i></a>
-                        <a class="btn btn-success" href="{{url('')}}/pdf_rj_izinvct"><i class="icon_check_alt2"></i></a>
+                        <a class="btn btn-success" href="{{url('')}}/pdf_ri_persetujuan_umum"><i class="icon_check_alt2"></i></a>
                         <a class="btn btn-danger" href="{{url('')}}/pdf_ri_hakkewajiban"><i class="icon_close_alt2"></i></a>
                       </div>
                     </td>
