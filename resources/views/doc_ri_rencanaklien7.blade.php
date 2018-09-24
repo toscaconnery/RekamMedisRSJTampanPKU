7e2f0bd327c0afd3cd26fc1cd59a9bcdf993951f
DESKTOP-KI75MOF\USER <auliasyauki@gmail.com>
2018-09-24 14:18:21 +0800

    All rencana klien has been finished

--- resources/views/doc_ri_rencanaklien7.blade.php
+++ resources/views/doc_ri_rencanaklien7.blade.php
@@ -11,94 +11,149 @@ header('Content-Type: application/pdf');
 
 <body style="">
 
-<h2 align="center" style="font-size:100%">RENCANA TINDAKAN KEPERAWATAN<br>KLIEN DENGAN PERILAKU KEKERASAN</h2>
+<h2 align="center" style="font-size:100%">RENCANA TINDAKAN KEPERAWATAN<br>KLIEN DENGAN RISIKO BUNUH DIRI</h2>
 
 <table>
   <thead>
     <tr>
-     <th class="big-box" width="8%" rowspan="2">Tgl</th>
-     <th class="big-box" width="8%" rowspan="2">No Dx</th> 
-     <th class="big-box" width="20%" rowspan="2">Dx Keperawatan</th> 
-     <th class="big-box" width="64%" colspan="3">Perencanaan</th>  
+     <th class="box-c" width="8%" rowspan="2">Tgl</th>
+     <th class="box-c" width="8%" rowspan="2">No Dx</th> 
+     <th class="box-c" width="20%" rowspan="2">Dx Keperawatan</th> 
+     <th class="box-c" width="64%" colspan="3">Perencanaan</th>  
     </tr>
     <tr>
-      <th class="big-box" width="16%">Tujuan</th>
-      <th class="big-box" width="20%">Kriteria Evaluasi</th>
-      <th class="big-box" width="27%">Intervensi</th>
+      <th class="box-c" width="16%">Tujuan</th>
+      <th class="box-c" width="20%">Kriteria Evaluasi</th>
+      <th class="box-c" width="27%">Intervensi</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
+      <td class="box-top"><br></td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
     </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
+    <tr>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">Risiko Bunuh diri</td>
+      <td class="box-top">
+        TUM: ...<br>
+        TUK 1: Klien dapat membina hubungan saling percaya
+      </td>
+      <td class="box-top">
+        1. Setelah ... X interaksi klien menunjukkan tanda-tanda percaya kepada perawat:
+        <br>
+        <ul>
+          <li>Ekspresi wajah bersahabat.</li>
+          <li>Menunjukkan rasa senang.</li>
+          <li>Ada kontak mata.</li>
+          <li>Mau berjabat tangan.</li>
+          <li>Mau menyebutkan nama.</li>
+          <li>Mau menjawab salam.</li>
+          <li>Mau duduk berdampingan dengan perawat.</li>
+          <li>Bersedia mengungkapkan masalah yang dihadapi</li>
+        </ul>
+      </td>
+      <td class="box-top">
+        1. Bina hubungan saling percaya dengan menggunakan prinsip komunikasi terapeutik:<br>
+        <ul>
+          <li>Sapa klien dengan ramah baik verbal maupun non verbal</li>
+          <li>Perkenakan nama, nama panggilan dan tujuan perawat berkenalan</li>
+          <li>Tanyakan nama lengkap dan nama panggilan yang disukai klien</li>
+          <li>Buat kontrak yang jelas</li>
+          <li>Tunjukkan sikap jujur dan menepati janji setiap kali interaksi</li>
+          <li>Tunjukkan sikap empati dan menerima apa adanya</li>
+          <li>Beri perhatian kepada klien dan perhatikan kebutuhan dasar klien</li>
+          <li>Tanyakan perasaan klien dan masalah yang dihadapi klien</li>
+          <li>Dengarkan dengan penuh perhatian ekspresi perasaan klien</li>
+        </ul>
+      </td>
     </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
+    <tr>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">TUK 2: Klien dapat dilindungi dari tindakan yang mengancam atau mencoba bunuh diri</td>
+      <td class="box-top">
+          2. Setelah ... X interaksi klien dapat dilindungi sampai tidak ada keinginan bunuh diri<br>
+          <ul>
+            <li>Ada kontak mata</li>
+            <li>Bersedia menceritakan perasaan</li>
+            <li>Menceritakan penyebab</li>
+            <li>Timbulnya keinginan bunuh diri baik dari diri sendiri maupun lingkungannya</li>
+          </ul>
+      </td>
+      <td class="box-top">
+      2.1. Identifikasi benda-benda yang dapa membahayakan pasien (pisau, silet, gelas, tali pinggang, alat kasur, dll)<br>
+      2.2. Amankan benda-benda yang dapat membahayakan pasien<br>
+      2.3. Lakukan kontrak <i>treatment</i><br>
+      2.4. Mengajarkan cara mengendalikan dorongan bunuh diri<br>
+      2.5. Melatih cara mengendalikan dorongan bunuh diri
+      </td>
     </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
+    <tr>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">TUK 3: Klien dapat meningkatkan harga diri</td>
+      <td class="box-top">
+          3. Setelah ... X interaksi klien menyebutkan:<br>
+          <ul>
+            <li>Aspek positif dari kemampuan yang dimiliki klien.</li>
+            <li>Aspek positif keluarga.</li>
+            <li>Aspek positif lingkungan klien</li>
+          </ul>
+      </td>
+      <td class="box-top">
+        3.1. Identifikasi aspek positif pasien<br>
+        3.2. Beri kesempatan pasien mengungkapkan perasaannya<br>
+        3.3. Dorong pasien untuk berpikir positif terhadap diri<br>
+        3.4. Berikan pujian bila pasien dapat mengatakan perasaan yang positif<br>
+        3.5. Yakinkan pasien bahwa dirinya penting<br>
+        3.6. Bicarakan tentang keadaan yang sepatutnya disyukuri oleh pasien<br>
+        3.7. Dorong pasien untuk menghargai diri sebagai individu yang berharga<br>
+      </td>
     </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
+    <tr>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">TUK 4: Klien dapat meningkatkan kemampuan koping dalam penyelesaian masalah</td>
+      <td class="box-top">
+          4. Setelah ... X interaksi klien menyebutkan:<br>
+          <ul>
+            <li>Koping konstruktif yang dapat digunkan untuk menyelesaikan masalah</li>
+            <li>Efektifitas penggunaan koping</li>
+          </ul>
+      </td>
+      <td class="box-top">
+          4.1. Identifikasi pola koping yang biasa diterapkan pasien<br>
+          4.2. Bantu untuk menilai pola koping yang biasa dilakukan<br>
+          4.3. Mengidentifikasi pola koping yang konstruktif<br>
+          4.4. Dorong pasien memilih pola koping yang konstruktif<br>
+          4.5. Anjurkan pasien menerapkan pola koping konstruktif dalam kegiatan harian 
+      </td>
     </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-    </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-    </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-    </tr>
-     <tr>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
-      <td class="box">-</td>
+    <tr>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">-</td>
+      <td class="box-top">TUK 5: Klien dapat merencanakan masa depan</td>
+      <td class="box-top">
+        5. Setelah ... X interaksi klien menyebutkan:<br>
+        <ul>
+          <li>Rencana masa depan yang sesuai kemampuan</li>
+        </ul>
+      </td>
+      <td class="box-top">
+        5.1. Diskusikan rencana masa depan yang realistis bersama pasien<br>
+        5.2. Identifikasi cara mencapai rencana masa depan yang realistis<br>
+        5.3. Membuat dorongan pasien melakukan kegiatan dalam rangka meraih masa depan yang realistis
+      </td>
     </tr>
   </tbody>
 </table>
