9c36e0467df0d2232205bc519f15be248993af1d
DESKTOP-KI75MOF\USER <auliasyauki@gmail.com>
2018-11-18 14:16:36 +0800

    add 3 new pdf

--- app/Http/Controllers/RIEdukasiController.php
+++ app/Http/Controllers/RIEdukasiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\RIEdukasi;
 use Session;
+use View;
 
 class RIEdukasiController extends Controller
 {
@@ -181,5 +182,132 @@ class RIEdukasiController extends Controller
         $this->data['dt_paraf_pasien'] = $pasien->dt_paraf_pasien;
         $this->data['dt_paraf_edukator'] = $pasien->dt_paraf_edukator;
         return view('page.ri.edukasi_read', $this->data);
+    }
+
+    public function ri_edukasi_pdf()
+    {
+    	$pasien = RIEdukasi::where('id', 1)->first();
+    	
+        $this->data['medis_materi'] = $pasien->medis_materi;
+        $this->data['medis_metode'] = $pasien->medis_metode;
+        $this->data['medis_nama_edukator'] = $pasien->medis_nama_edukator;
+        $this->data['medis_paraf_pasien'] = $pasien->medis_paraf_pasien;
+        $this->data['medis_paraf_edukator'] = $pasien->medis_paraf_edukator;
+
+        $this->data['mj_materi'] = $pasien->mj_materi;
+        $this->data['mj_metode'] = $pasien->mj_metode;
+        $this->data['mj_nama_edukator'] = $pasien->mj_nama_edukator;
+        $this->data['mj_paraf_pasien'] = $pasien->mj_paraf_pasien;
+        $this->data['mj_paraf_edukator'] = $pasien->mj_paraf_edukator;
+
+        $this->data['psikologis_materi'] = $pasien->psikologis_materi;
+        $this->data['psikologis_metode'] = $pasien->psikologis_metode;
+        $this->data['psikologis_nama_edukator'] = $pasien->psikologis_nama_edukator;
+        $this->data['psikologis_paraf_pasien'] = $pasien->psikologis_paraf_pasien;
+        $this->data['psikologis_paraf_edukator'] = $pasien->psikologis_paraf_edukator;
+
+        $this->data['keperawatan_materi'] = $pasien->keperawatan_materi;
+        $this->data['keperawatan_metode'] = $pasien->keperawatan_metode;
+        $this->data['keperawatan_nama_edukator'] = $pasien->keperawatan_nama_edukator;
+        $this->data['keperawatan_paraf_pasien'] = $pasien->keperawatan_paraf_pasien;
+        $this->data['keperawatan_paraf_edukator'] = $pasien->keperawatan_paraf_edukator;
+
+        $this->data['farmasi_materi'] = $pasien->farmasi_materi;
+        $this->data['farmasi_metode'] = $pasien->farmasi_metode;
+        $this->data['farmasi_nama_edukator'] = $pasien->farmasi_nama_edukator;
+        $this->data['farmasi_paraf_pasien'] = $pasien->farmasi_paraf_pasien;
+        $this->data['farmasi_paraf_edukator'] = $pasien->farmasi_paraf_edukator;
+
+        $this->data['nutrisi_materi'] = $pasien->nutrisi_materi;
+        $this->data['nutrisi_metode'] = $pasien->nutrisi_metode;
+        $this->data['nutrisi_nama_edukator'] = $pasien->nutrisi_nama_edukator;
+        $this->data['nutrisi_paraf_pasien'] = $pasien->nutrisi_paraf_pasien;
+        $this->data['nutrisi_paraf_edukator'] = $pasien->nutrisi_paraf_edukator;
+
+        $this->data['kerohanian_materi'] = $pasien->kerohanian_materi;
+        $this->data['kerohanian_metode'] = $pasien->kerohanian_metode;
+        $this->data['kerohanian_nama_edukator'] = $pasien->kerohanian_nama_edukator;
+        $this->data['kerohanian_paraf_pasien'] = $pasien->kerohanian_paraf_pasien;
+        $this->data['kerohanian_paraf_edukator'] = $pasien->kerohanian_paraf_edukator;
+
+        $this->data['rehabilitasi_materi'] = $pasien->rehabilitasi_materi;
+        $this->data['rehabilitasi_metode'] = $pasien->rehabilitasi_metode;
+        $this->data['rehabilitasi_nama_edukator'] = $pasien->rehabilitasi_nama_edukator;
+        $this->data['rehabilitasi_paraf_pasien'] = $pasien->rehabilitasi_paraf_pasien;
+        $this->data['rehabilitasi_paraf_edukator'] = $pasien->rehabilitasi_paraf_edukator;
+
+        $this->data['dt_materi'] = $pasien->dt_materi;
+        $this->data['dt_metode'] = $pasien->dt_metode;
+        $this->data['dt_evaluasi'] = $pasien->dt_evaluasi;
+        $this->data['dt_nama_edukator'] = $pasien->dt_nama_edukator;
+        $this->data['dt_paraf_pasien'] = $pasien->dt_paraf_pasien;
+        $this->data['dt_paraf_edukator'] = $pasien->dt_paraf_edukator;
+
+        ob_clean();
+
+        header('Content-type: application/pdf');
+
+        $mpdf = new \Mpdf\Mpdf([
+        'mode' => 'utf-8', 
+        'format' => 'A4-L', 
+        'orientation' => 'P'
+        ]);
+
+        $view = View::make('doc_ri_edukasi',$this->data);
+        $contents = $view->render();
+
+
+        $mpdf->SetHTMLHeader('
+            <table width="100%" >
+                <tbody>
+                        <tr>
+
+                            <td class= "doc_headerleft">
+                                <img class="relative" src="img/riau.png" height="9%" width="7%">
+                            </td>
+
+                            <td class="doc_headermid">
+                                <p style="font-size:120%;" ><b>Pemerintah Provinsi Riau</b></p>
+                                <br><p style="font-size:160%;" ><b>RUMAH SAKIT JIWA TAMPAN</b></p>
+                                <br><p style="font-size:90%;" >Jl. H. R. Soebrantas Km. 12,5 Pekanbaru Telp. (0761) 63240</p>
+                                <p style="font-size:90%;" >Fax. (0761) 63239 E-mail : rstampan@yahoo.com</p>
+                            </td>
+                            
+                            <td class="doc_headerright">
+                            <p>No. RM           </p><br>
+                            <p>Nama Pasien      </p><br>
+                            <p>Tanggal Lahir    </p><br>
+                            <p>Jenis Kelamin    </p><br>
+                            </td>    
+
+                            <td class="doc_headerright_ans">
+                            <p>:  123456</p><br>
+                            <p>:  Joko Pangestu</p><br>
+                            <p>:  13/08/1992</p><br>
+                            <p>:  L</p><br>
+                            </td>                       
+                        </tr>
+  
+                </tbody>
+            </table> <hr>' );
+
+        $mpdf->SetHTMLFooter('<hr>
+            <table width="100%" style="vertical-align: bottom; font-family: "arial", Times, serif; 
+                font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
+                <tr>
+                    <td width="33%">{DATE j-m-Y}</td>
+                    <td width="33%" align="center">{PAGENO}/{nbpg}</td>
+                    <td width="33%" style="text-align: right;">RM. 30.00.RI JULI 2015</td>
+                </tr>
+            </table>'); 
+
+        $stylesheet = '<style>'.file_get_contents('css/pdf.css').'</style>';
+
+        $mpdf->AddPage('P','','','','',10,10,35,20,10,10);
+        //right,left
+        $mpdf->WriteHTML($stylesheet, 1);
+        $mpdf->WriteHTML($contents,2,true,false);
+
+        $mpdf->Output('ri_edukasi.pdf',\Mpdf\Output\Destination::INLINE);
     }
 }
--- app/Http/Controllers/RISuicideFisikController.php
+++ app/Http/Controllers/RISuicideFisikController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\RISuicideFisik;
 use Session;
+use View;
 
 class RISuicideFisikController extends Controller
 {
@@ -118,4 +119,136 @@ class RISuicideFisikController extends Controller
         return view('page.ri.suicide_fisik_read', $this->data);
 
     }
+
+    public function answer($answer)
+    {
+        if($answer=='1')
+        {
+            return 'Yes';
+        }
+        else if($answer=='2')
+        {
+            return 'No';
+        }
+        else
+        {
+            return 'Not Known';
+        } 
+    }
+
+    public function ri_suicide_pdf()
+    {
+        $pasien = RISuicideFisik::where('id', 1)->first();
+        
+        $this->data['id_regis'] = $pasien->id_regis;
+
+        $this->data['s1'] = $this->answer($pasien->s1);
+        $this->data['s2'] = $this->answer($pasien->s2);
+        $this->data['s3'] = $this->answer($pasien->s3);
+        $this->data['s4'] = $this->answer($pasien->s4);
+        $this->data['s5'] = $this->answer($pasien->s5);
+        $this->data['s6'] = $this->answer($pasien->s6);
+        $this->data['s7'] = $this->answer($pasien->s7);
+        $this->data['s8'] = $this->answer($pasien->s8);
+        $this->data['s9'] = $this->answer($pasien->s9);
+        $this->data['s10'] = $this->answer($pasien->s10);
+
+        $this->data['s11'] = $this->answer($pasien->s11);
+        $this->data['s12'] = $this->answer($pasien->s12);
+        $this->data['s13'] = $this->answer($pasien->s13);
+        $this->data['s14'] = $this->answer($pasien->s14);
+        $this->data['s15'] = $this->answer($pasien->s15);
+        $this->data['s16'] = $this->answer($pasien->s16);
+        $this->data['s17'] = $this->answer($pasien->s17);
+        $this->data['s18'] = $this->answer($pasien->s18);
+        $this->data['s19'] = $this->answer($pasien->s19);
+        $this->data['s20'] = $this->answer($pasien->s20);
+
+        $this->data['s21'] = $this->answer($pasien->s21);
+        $this->data['s22'] = $this->answer($pasien->s22);
+        $this->data['s23'] = $this->answer($pasien->s23);
+        $this->data['s24'] = $this->answer($pasien->s24);
+        $this->data['s25'] = $this->answer($pasien->s25);
+        $this->data['s26'] = $this->answer($pasien->s26);
+        $this->data['s27'] = $this->answer($pasien->s27);
+        $this->data['s28'] = $this->answer($pasien->s28);
+        $this->data['s29'] = $this->answer($pasien->s29);
+        $this->data['s30'] = $this->answer($pasien->s30);
+
+        $this->data['s31'] = $this->answer($pasien->s31);
+        $this->data['s32'] = $this->answer($pasien->s32);
+        $this->data['s33'] = $this->answer($pasien->s33);
+        
+        $this->data['p1'] = $pasien->p1;
+        $this->data['p2'] = $pasien->p2;
+        $this->data['p3'] = $pasien->p3;
+
+        ob_clean();
+
+        header('Content-type: application/pdf');
+
+        $mpdf = new \Mpdf\Mpdf([
+        'mode' => 'utf-8', 
+        'format' => 'A4-L', 
+        'orientation' => 'P'
+        ]);
+
+        $view = View::make('doc_ri_suicide',$this->data);
+        $contents = $view->render();
+
+
+        $mpdf->SetHTMLHeader('
+            <table width="100%" >
+                <tbody>
+                        <tr>
+
+                            <td class= "doc_headerleft">
+                                <img class="relative" src="img/riau.png" height="9%" width="7%">
+                            </td>
+
+                            <td class="doc_headermid">
+                                <p style="font-size:120%;" ><b>Pemerintah Provinsi Riau</b></p>
+                                <br><p style="font-size:160%;" ><b>RUMAH SAKIT JIWA TAMPAN</b></p>
+                                <br><p style="font-size:90%;" >Jl. H. R. Soebrantas Km. 12,5 Pekanbaru Telp. (0761) 63240</p>
+                                <p style="font-size:90%;" >Fax. (0761) 63239 E-mail : rstampan@yahoo.com</p>
+                            </td>
+                            
+                            <td class="doc_headerright">
+                            <p>No. RM           </p><br>
+                            <p>Nama Pasien      </p><br>
+                            <p>Tanggal Lahir    </p><br>
+                            <p>Jenis Kelamin    </p><br>
+                            </td>    
+
+                            <td class="doc_headerright_ans">
+                            <p>:  123456</p><br>
+                            <p>:  Joko Pangestu</p><br>
+                            <p>:  13/08/1992</p><br>
+                            <p>:  L</p><br>
+                            </td>                       
+                        </tr>
+  
+                </tbody>
+            </table> <hr>' );
+
+        $mpdf->SetHTMLFooter('<hr>
+            <table width="100%" style="vertical-align: bottom; font-family: "arial", Times, serif; 
+                font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
+                <tr>
+                    <td width="33%">{DATE j-m-Y}</td>
+                    <td width="33%" align="center">{PAGENO}/{nbpg}</td>
+                    <td width="33%" style="text-align: right;">RM. 14.00.RI JULI 2015</td>
+                </tr>
+            </table>'); 
+
+        $stylesheet = '<style>'.file_get_contents('css/pdf.css').'</style>';
+
+        $mpdf->AddPage('P','','','','',10,10,35,20,10,10);
+        //right,left
+        $mpdf->WriteHTML($stylesheet, 1);
+        $mpdf->WriteHTML($contents,2,true,false);
+
+        $mpdf->Output('ri_suicide.pdf',\Mpdf\Output\Destination::INLINE);
+    }
+
 }
