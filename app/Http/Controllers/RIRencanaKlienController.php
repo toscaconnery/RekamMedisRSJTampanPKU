0ada3e0903700eb068f1ef13c2f53b3747ffe2f5
toscaconnery <toscayoelconnery@gmail.com>
2018-11-27 20:24:58 +0700

    rencana tindakan klien 6 edit read

--- app/Http/Controllers/RIRencanaKlienController.php
+++ app/Http/Controllers/RIRencanaKlienController.php
@@ -711,4 +711,157 @@ class RIRencanaKlienController extends Controller
 		$data->save();
 		return redirect('ri_rencana_klien_5_read');
     }
+
+    public function get_ri_rencana_klien_6()
+    {
+    	$this->data['title'] = 'Rencana Tindakan Keperawatan Klien Dengan Gangguan Persepsi Sensori : Halusinasi';
+    	return view('page.ri.rencana_klien_6', $this->data);
+    }
+
+    public function post_ri_rencana_klien_6(Request $request)
+    {
+    	$id_pasien = Session::get('id_pasien');
+    	$data = new RIRencanaKlien6;
+    	$data->id_regis = $id_pasien;
+    	$data->tanggal_pengkajian = $request->tanggal_pengkajian;
+		$data->jam_pengkajian = $request->jam_pengkajian;
+		$data->ruangan = $request->ruangan;
+		$data->diagnosa_medis = $request->diagnosa_medis;
+		$data->tanggal_1 = $request->tanggal_1;
+		$data->tanggal_2 = $request->tanggal_2;
+		$data->tanggal_2b = $request->tanggal_2b;
+		$data->tanggal_3 = $request->tanggal_3;
+		$data->tanggal_4 = $request->tanggal_4;
+		$data->tanggal_5 = $request->tanggal_5;
+		$data->nodx_1 = $request->nodx_1;
+		$data->nodx_2 = $request->nodx_2;
+		$data->nodx_2b = $request->nodx_2b;
+		$data->nodx_3 = $request->nodx_3;
+		$data->nodx_4 = $request->nodx_4;
+		$data->nodx_5 = $request->nodx_5;
+		$data->dxk_1 = $request->dxk_1;
+		$data->dxk_2 = $request->dxk_2;
+		$data->dxk_2b = $request->dxk_2b;
+		$data->dxk_3 = $request->dxk_3;
+		$data->dxk_4 = $request->dxk_4;
+		$data->dxk_5 = $request->dxk_5;
+		$data->t1 = $request->t1;
+		$data->k1 = $request->k1;
+		$data->k2 = $request->k2;
+		$data->k2b = $request->k2b;
+		$data->k3 = $request->k3;
+		$data->k3b = $request->k3b;
+		$data->k3c = $request->k3c;
+		$data->k3d = $request->k3d;
+		$data->k3e = $request->k3e;
+		$data->k4 = $request->k4;
+		$data->k4b = $request->k4b;
+		$data->k5 = $request->k5;
+		$data->k5b = $request->k5b;
+		$data->k5c = $request->k5c;
+		$data->save();
+		return redirect('ri_rencana_klien_6_read');
+    }
+
+    public function get_ri_rencana_klien_6_data()
+    {
+    	$id_pasien = Session::get('id_pasien');
+    	$pasien = RIRencanaKlien6::where('id_regis', $id_pasien)->first();
+
+    	$this->data['tanggal_pengkajian'] = $pasien->tanggal_pengkajian;
+		$this->data['jam_pengkajian'] = $pasien->jam_pengkajian;
+		$this->data['ruangan'] = $pasien->ruangan;
+		$this->data['diagnosa_medis'] = $pasien->diagnosa_medis;
+		$this->data['tanggal_1'] = $pasien->tanggal_1;
+		$this->data['tanggal_2'] = $pasien->tanggal_2;
+		$this->data['tanggal_2b'] = $pasien->tanggal_2b;
+		$this->data['tanggal_3'] = $pasien->tanggal_3;
+		$this->data['tanggal_4'] = $pasien->tanggal_4;
+		$this->data['tanggal_5'] = $pasien->tanggal_5;
+		$this->data['nodx_1'] = $pasien->nodx_1;
+		$this->data['nodx_2'] = $pasien->nodx_2;
+		$this->data['nodx_2b'] = $pasien->nodx_2b;
+		$this->data['nodx_3'] = $pasien->nodx_3;
+		$this->data['nodx_4'] = $pasien->nodx_4;
+		$this->data['nodx_5'] = $pasien->nodx_5;
+		$this->data['dxk_1'] = $pasien->dxk_1;
+		$this->data['dxk_2'] = $pasien->dxk_2;
+		$this->data['dxk_2b'] = $pasien->dxk_2b;
+		$this->data['dxk_3'] = $pasien->dxk_3;
+		$this->data['dxk_4'] = $pasien->dxk_4;
+		$this->data['dxk_5'] = $pasien->dxk_5;
+		$this->data['t1'] = $pasien->t1;
+		$this->data['k1'] = $pasien->k1;
+		$this->data['k2'] = $pasien->k2;
+		$this->data['k2b'] = $pasien->k2b;
+		$this->data['k3'] = $pasien->k3;
+		$this->data['k3b'] = $pasien->k3b;
+		$this->data['k3c'] = $pasien->k3c;
+		$this->data['k3d'] = $pasien->k3d;
+		$this->data['k3e'] = $pasien->k3e;
+		$this->data['k4'] = $pasien->k4;
+		$this->data['k4b'] = $pasien->k4b;
+		$this->data['k5'] = $pasien->k5;
+		$this->data['k5b'] = $pasien->k5b;
+		$this->data['k5c'] = $pasien->k5c;
+    }
+
+    public function get_ri_rencana_klien_6_read()
+    {
+    	$this->data['title'] = 'Rencana Tindakan Keperawatan Klien Dengan Gangguan Persepsi Sensori : Halusinasi';
+    	$this->get_ri_rencana_klien_6_data();
+    	return view('page.ri.rencana_klien_6_read', $this->data);
+    }
+
+    public function get_ri_rencana_klien_6_edit()
+    {
+    	$this->data['title'] = 'Rencana Tindakan Keperawatan Klien Dengan Gangguan Persepsi Sensori : Halusinasi';
+    	$this->get_ri_rencana_klien_6_data();
+    	return view('page.ri.rencana_klien_6_edit', $this->data);
+    }
+
+    public function post_ri_rencana_klien_6_edit(Request $request)
+    {
+    	$id_pasien = Session::get('id_pasien');
+    	$data = RIRencanaKlien6::where('id_regis', $id_pasien)->first();
+
+    	$data->tanggal_pengkajian = $request->tanggal_pengkajian;
+		$data->jam_pengkajian = $request->jam_pengkajian;
+		$data->ruangan = $request->ruangan;
+		$data->diagnosa_medis = $request->diagnosa_medis;
+		$data->tanggal_1 = $request->tanggal_1;
+		$data->tanggal_2 = $request->tanggal_2;
+		$data->tanggal_2b = $request->tanggal_2b;
+		$data->tanggal_3 = $request->tanggal_3;
+		$data->tanggal_4 = $request->tanggal_4;
+		$data->tanggal_5 = $request->tanggal_5;
+		$data->nodx_1 = $request->nodx_1;
+		$data->nodx_2 = $request->nodx_2;
+		$data->nodx_2b = $request->nodx_2b;
+		$data->nodx_3 = $request->nodx_3;
+		$data->nodx_4 = $request->nodx_4;
+		$data->nodx_5 = $request->nodx_5;
+		$data->dxk_1 = $request->dxk_1;
+		$data->dxk_2 = $request->dxk_2;
+		$data->dxk_2b = $request->dxk_2b;
+		$data->dxk_3 = $request->dxk_3;
+		$data->dxk_4 = $request->dxk_4;
+		$data->dxk_5 = $request->dxk_5;
+		$data->t1 = $request->t1;
+		$data->k1 = $request->k1;
+		$data->k2 = $request->k2;
+		$data->k2b = $request->k2b;
+		$data->k3 = $request->k3;
+		$data->k3b = $request->k3b;
+		$data->k3c = $request->k3c;
+		$data->k3d = $request->k3d;
+		$data->k3e = $request->k3e;
+		$data->k4 = $request->k4;
+		$data->k4b = $request->k4b;
+		$data->k5 = $request->k5;
+		$data->k5b = $request->k5b;
+		$data->k5c = $request->k5c;
+		$data->save();
+		return redirect('ri_rencana_klien_6_read');
+    }
 }
