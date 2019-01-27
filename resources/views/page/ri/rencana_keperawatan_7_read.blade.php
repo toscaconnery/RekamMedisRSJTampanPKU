02125a1a55e9bd940207c57789b3cd25cc0f585d
DESKTOP-KI75MOF\USER <auliasyauki@gmail.com>
2019-01-27 22:32:26 +0700

    rencana rencana rencana, go to revision, some page need fixing

--- resources/views/page/ri/rencana_keperawatan_7_read.blade.php
+++ resources/views/page/ri/rencana_keperawatan_7_read.blade.php
@@ -23,9 +23,6 @@
       <div class="row">
         <div class="col-lg-12">
           <section class="panel">
-            <header class="panel-heading">
-              Rencana Tindakan Keperawatan Intensif Isolasi Sosial: Menarik Diri
-            </header>
 
             <table class="table table-striped table-advance table-hover">
               <tbody>
@@ -33,7 +30,8 @@
                   <th><i class="icon_document_alt"></i> Dokumen</th>
                   <th><i class="icon_calendar"></i> Tanggal Pengisian</th>
                   <th><i class="icon_profile"></i> Pengisi</th>
-                  <th><i class="icon_cogs"></i> Action</th>
+                  <th><i class="icon_cogs"></i> Aksi</th>
+                  <th><i class="icon_document"></i> Cetak Dokumen</th>
                 </tr>
                 <tr>
                   <td>Rencana Tindakan Keperawatan Intensif Isolasi Sosial: Menarik Diri</td>
@@ -41,9 +39,14 @@
                   <td>[Nama Pengisi]</td>
                   <td>
                     <div class="btn-group">
-                      <a class="btn btn-primary" href="{{url('')}}/ri_rencana7_pdf">PDF</a>
-                      <a class="btn btn-success" href="#">EDIT</a>
+                      <a class="btn btn-info" href="{{url('')}}/ri_rencana_keperawatan_7">Isi</a>
+                      <a class="btn btn-primary" href="{{url('')}}/ri_rencana_keperawatan_7_read">Lihat</a>
+                      <a class="btn btn-success" href="{{url('')}}/ri_rencana_keperawatan_7_edit">Edit</i></a>
+                      <a class="btn btn-danger" href="#">Hapus</a>
                     </div>
+                  </td>
+                  <td>
+                    <a class="btn btn-default" href="{{url('')}}/ri_rencana7_pdf">Cetak</a>
                   </td>
                 </tr>
               </tbody>
--- resources/views/page/ri/rencana_napza.blade.php
+++ resources/views/page/ri/rencana_napza.blade.php
@@ -23,16 +23,14 @@
       <div class="row">
         <div class="col-lg-12">
           <section class="panel">
-            <header class="panel-heading">
-              Rencana Tindakan Keperawatan Napza
-            </header>
             <table class="table table-striped table-advance table-hover">
               <tbody>
                 <tr>
                   <th><i class="icon_document_alt"></i> Dokumen</th>
                   <th><i class="icon_calendar"></i> Tanggal Pengisian</th>
                   <th><i class="icon_profile"></i> Pengisi</th>
-                  <th><i class="icon_cogs"></i> Action</th>
+                  <th><i class="icon_cogs"></i> Aksi</th>
+                  <th><i class="icon_document"></i> Cetak Dokumen</th>
                 </tr>
                 <tr>
                   <td>Rencana Tindakan Keperawatan Napza</td>
@@ -40,10 +38,14 @@
                   <td>[Nama Pengisi]</td>
                   <td>
                     <div class="btn-group">
-                      <a class="btn btn-primary" href="#"><i class="icon_plus_alt2"></i></a>
-                      <a class="btn btn-success" href="#"><i class="icon_check_alt2"></i></a>
-                      <a class="btn btn-danger" href="#"><i class="icon_close_alt2"></i></a>
+                      <a class="btn btn-info" href="{{url('')}}/ri_rencana_napza">Isi</a>
+                      <a class="btn btn-primary" href="{{url('')}}/ri_rencana_napza_read">Lihat</a>
+                      <a class="btn btn-success" href="{{url('')}}/ri_rencana_napza_edit">Edit</i></a>
+                      <a class="btn btn-danger" href="#">Hapus</a>
                     </div>
+                  </td>
+                  <td>
+                    <a class="btn btn-default" href="{{url('')}}/ri_rawat_napza_pdf">Cetak</a>
                   </td>
                 </tr>
               </tbody>
